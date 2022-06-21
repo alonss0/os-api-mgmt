08fb1064e626f88880bad68707f35905989faccf
Alonso <cardenasjairods@gmail.com>
2022-06-20 20:24:03 -0500

    creating basic express server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # os-api-mgmt
-The purpose of this project is to interact with OpenSea API management to retrieve assets and orders.
+The purpose of this project is to interact with OpenSea API to retrieve assets and orders, saving data in mongodb and creating a service. 
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const router = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', router);
+
+const PORT = 3003 || process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🔥`);
+})
