544b909577779958ef005045c8d25b26cce7b484
Alonso <cardenasjairods@gmail.com>
2022-08-09 10:52:19 -0500

    FEAT: creation of route upload-csv

--- routes/service_routes.js
+++ routes/service_routes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const Mongoose = require('mongoose');
 const collSchema = require('../schemas/collSchema');
 const Web3 = require('web3');
+const fs = require('fs');
+const multer = require('multer');
+const csv = require('fast-csv');
 
 const router = express.Router();
+
+const upload = multer({ dest: 'tmp/csv/' });
 
 function containsDuplicates(array) {
     const result = array.some(element => {
@@ -14,6 +19,45 @@ function containsDuplicates(array) {
     });
     return result;
 }
+
+router.post('/upload-csv', upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No files were uploaded.');
+        } else if (req.query.collection == null) {
+            return res.status(400).send('Provide collection name');
+        } else {
+            const collectionName = Web3.utils.toChecksumAddress(req.query.collection);
+            const arr = await Mongoose.connection.db.listCollections().toArray();
+            if (arr.some(coll => Web3.utils.toChecksumAddress(coll.name) === collectionName)) {
+                const fileRows = [];
+                csv.parseFile(req.file.path)
+                    .on("data", (data) => {
+                        fileRows.push({
+                            token_id: data[0],
+                        });
+                    })
+                    .on("end", () => {
+                        fileRows.shift();
+                        const model = Mongoose.model(collectionName, collSchema);
+                        model.insertMany(fileRows)
+                            .then(() => {
+                                res.send(fileRows.length + ' token_ids have been succesfully uploaded to collection ' + collectionName);
+                            })
+                            .catch((error) => {
+                                if (error) throw error;
+                            })
+                        fs.unlinkSync(req.file.path);
+                    });
+
+            } else {
+                res.send("Collection doesn't exists!");
+            }
+        }
+    } catch (error) {
+        res.status(500).send(`${error}`);
+    }
+})
 
 router.post('/addCollection', async (req, res) => {
     try {
@@ -45,7 +89,7 @@ router.post('/addTokens', async (req, res) => {
                     token_id: req.query.token_id
                 })
                 if (response.length != 0) {
-                    res.send({"The next tokens already exists!": response});
+                    res.send({ "The next tokens already exists!": response });
                 } else {
                     if (containsDuplicates(req.query.token_id)) {
                         res.send('Check token id params, duplicate values');
@@ -79,7 +123,7 @@ router.get('/getData', async (req, res) => {
                 const response = await model.find({
                     token_id: req.query.token_id
                 })
-                res.send({results: response});
+                res.send({ results: response });
             } else {
                 res.send("Collection doesn't exists!");
             }
